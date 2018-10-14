a387a05e32fc651e7959befa9a37056b7108289b
zemlyansky <anton.zemlyansky@gmail.com>
2018-10-14 21:36:42 +0300

    Add getters

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ module.exports = function (params) {
   var n = 0
   var mean = 0
   var m2 = 0
-  var variance = 0
+  var value = 0
   var ddof = params.ddof || 0
 
-  return function (x) {
+  var variance = function variance (x, seriesObject) {
     if (!isNaN(x)) {
       if (typeof x !== 'number') {
         x = parseFloat(x)
@@ -17,9 +17,23 @@ module.exports = function (params) {
       mean = mean + delta / n
       m2 = m2 + delta * (x - mean)
       if (n > 1) {
-        variance = m2 / (n - ddof)
+        value = m2 / (n - ddof)
       }
     }
-    return variance
+    return value
   }
+
+  Object.defineProperty(variance, 'value', {
+    get: function () {
+      return value
+    }
+  })
+
+  Object.defineProperty(variance, 'n', {
+    get: function () {
+      return n
+    }
+  })
+
+  return variance
 }
